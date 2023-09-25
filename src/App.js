e1a8758176bb7685b8366d11666e80a282a81e52
Paulo <paulodiazg32@gmail.com>
2023-09-25 12:20:51 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import Dropzone from 'react-dropzone';
+import { ReactP5Wrapper } from '@p5-wrapper/react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+
+  const handleImageDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const imageUrl = URL.createObjectURL(file);
+    setImage(imageUrl);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Editor with p5.js</h1>
+      <Dropzone onDrop={handleImageDrop} accept="image/*">
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} className="dropzone">
+            <input {...getInputProps()} />
+            <p>Drag &amp; drop an image here, or click to select one.</p>
+          </div>
+        )}
+      </Dropzone>
+      {image && <ImageEditor image={image} />}
     </div>
   );
 }
+
+const ImageEditor = ({ image }) => {
+  const imgRef = useRef(null);
+  const lines = [];
+  let startPoint = null;
+  let endPoint = null;
+  let currentColor = [0, 0, 0]; // Initial color: black
+
+  const sketch = (p) => {
+    let img;
+
+    p.preload = () => {
+      img = p.loadImage(image, () => {
+        p.createCanvas(img.width, img.height);
+        p.background(255);
+        p.image(img, 0, 0);
+      });
+    };
+
+    p.setup = () => {
+      p.noFill();
+      p.stroke(currentColor);
+      p.strokeWeight(2);
+      p.createCanvas(img.width, img.height); // Set canvas size to match image
+      p.background(255);
+      p.image(img, 0, 0);
+    };
+
+    p.draw = () => {
+      for (const line of lines) {
+        p.line(line[0], line[1], line[2], line[3]);
+      }
+
+      if (startPoint && endPoint) {
+        p.line(startPoint[0], startPoint[1], endPoint[0], endPoint[1]);
+      }
+    };
+
+    p.mousePressed = () => {
+      if (!startPoint) {
+        startPoint = [p.mouseX, p.mouseY];
+      } else if (!endPoint) {
+        endPoint = [p.mouseX, p.mouseY];
+        lines.push([...startPoint, ...endPoint]);
+        startPoint = null;
+        endPoint = null;
+      }
+    };
+  };
+
+  const setColor = (color) => {
+    currentColor = color;
+  };
+
+  return (
+    <div className="image-editor">
+      <img
+        ref={imgRef}
+        src={image}
+        alt="Uploaded"
+        style={{ display: 'none' }}
+      />
+      <div className="drawing-tools">
+        <button onClick={() => setColor([0, 0, 0])}>Black</button>
+        <button onClick={() => setColor([255, 0, 0])}>Red</button>
+        <button onClick={() => setColor([0, 255, 0])}>Green</button>
+        <button onClick={() => setColor([0, 0, 255])}>Blue</button>
+      </div>
+      <ReactP5Wrapper sketch={sketch} />
+    </div>
+  );
+};
 
 export default App;
